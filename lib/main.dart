591720d7fd0341040d485007a1c9b616fc4aa7de
Matheus Ferreira <matheusmw683@gmail.com>
2023-12-06 05:44:32 -0300

    mudancanomeapp2

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Promobit Beta 1.0',
+      title: 'Promobit Beta 1.5',
       theme: AppTheme.lightTheme(context),
       initialRoute: SplashScreen.routeName,
       routes: routes,
