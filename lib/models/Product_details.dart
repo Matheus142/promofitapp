2123afe79c06156504c00b610b45201c12695947
Matheus Ferreira <matheusmw683@gmail.com>
2023-12-12 06:28:40 -0300

    commit

--- lib/models/Product_details.dart
+++ lib/models/Product_details.dart
@@ -1,71 +1,124 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class ProductsDetailsPage extends StatelessWidget {
+class ProductsDetailsPage extends StatefulWidget {
   static const String routeName = '/products_details';
+
+  final List<String> productImages = [
+    "assets/images/wheyfrente.png",
+    "assets/images/wheycosta.png",
+    "assets/images/wheytabela.png",
+  ];
+
+  final String productDescription = "Whey Max Titanium de R\$105 por R\$95";
+
+  @override
+  _ProductsDetailsPageState createState() => _ProductsDetailsPageState();
+}
+
+class _ProductsDetailsPageState extends State<ProductsDetailsPage> {
+  String selectedImage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    selectedImage = widget.productImages.first;
+  }
+
+  Future<String> _createBitlyDynamicLink() async {
+    final String accessToken = 'bc0432e138fab71c60bb9d30830bfcb1d93e1b8d';
+    final String amazonLink = 'https://www.maxtitanium.com.br/100-whey-pote-900g/p?idsku=116&utm_source=google&utm_campaign=shopping&utm_medium=cpc&utm_source=v4google&utm_medium=pmax&utm_campaign=[V4][MO][PMA][GOO][GO]+Termogenicos-16275184680&utm_content=PMAX-&utm_term=&adid=&gad_source=1&gclid=CjwKCAiApuCrBhAuEiwA8VJ6Jkqbaiy88iOzrOT-hvBXXTmGQaO1lQ9ZQN2nS3LeTub5Bmi_vm0rdBoCs4MQAvD_BwE'; // Link específico do produto na Amazon
+
+    final response = await http.post(
+      Uri.parse('https://api-ssl.bitly.com/v4/shorten'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $accessToken',
+      },
+      body: jsonEncode({
+        'long_url': amazonLink,
+        'domain': 'bit.ly',
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['link'];
+    } else {
+      print('Erro ao criar o link dinâmico no Bitly: ${response.statusCode}');
+      print('Resposta do servidor: ${response.body}');
+      throw 'Erro ao criar o link dinâmico no Bitly';
+    }
+  }
+
+  Future<void> _openBitlyDynamicLink() async {
+    try {
+      final String dynamicLink = await _createBitlyDynamicLink();
+      if (dynamicLink.isNotEmpty) {
+        if (await canLaunch(dynamicLink)) {
+          await launch(dynamicLink);
+        } else {
+          throw 'Could not launch $dynamicLink';
+        }
+      } else {
+        throw 'Empty dynamic link';
+      }
+    } catch (e) {
+      print('Erro ao abrir o link: $e');
+      // Adicione aqui um código para lidar com o erro ao abrir o link dinâmico
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    String productImageUrl = "assets/images/wheyfrente.png";
-    String productDescription = "Descrição do Produto";
-    String amazonProductUrl =
-        "https://www.amazon.com.br/Max-Titanium-Forma%C3%A7%C3%A3o-Quantidade-Amino%C3%A1cidos/dp/B0BHFGGHDP/ref=asc_df_B0BHFGGHDP/?tag=googleshopp00-20&linkCode=df0&hvadid=405196231621&hvpos=&hvnetw=g&hvrand=13577095741229434362&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9102338&hvtargid=pla-2015859784508&mcid=9785574c13ea31f1bdc4f31ea5c6c7a3&th=17";
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Detalhes do Produto'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.4,
-              child: GestureDetector(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                widget.productDescription,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16),
+              GestureDetector(
                 onTap: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (_) => FullScreenImage(imageUrl: productImageUrl),
+                      builder: (context) =>
+                          FullScreenImage(imageUrl: selectedImage),
                     ),
                   );
                 },
                 child: Hero(
                   tag: 'product_image',
-                  child: Card(
-                    elevation: 3.0,
-                    child: Image.asset(
-                      productImageUrl,
-                      fit: BoxFit.cover,
-                    ),
+                  child: Image.asset(
+                    selectedImage,
+                    fit: BoxFit.cover,
+                    height: 200, // Altura ajustada para a visualização adequada
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 16),
-            // Description icon
-            GestureDetector(
-              onTap: () {
-                // Add your description logic here
-                print('Description tapped');
-              },
-              child: const Icon(Icons.description),
-            ),
-            const SizedBox(height: 16),
-            // Validate offer button
-            ElevatedButton(
-              onPressed: () async {
-                if (await canLaunch(amazonProductUrl)) {
-                  await launch(amazonProductUrl);
-                } else {
-                  throw 'Could not launch $amazonProductUrl';
-                }
-              },
-              child: const Text('Eu quero validar a oferta'),
-            ),
-            const SizedBox(height: 16),
-          ],
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _openBitlyDynamicLink,
+                child: Text('Clique para validar a oferta'),
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
         ),
       ),
     );
@@ -89,7 +142,9 @@ class FullScreenImage extends StatelessWidget {
             tag: 'product_image',
             child: Image.asset(
               imageUrl,
-              fit: BoxFit.cover,
+              fit: BoxFit.contain,
+              height:
+                  MediaQuery.of(context).size.height, // Usar toda a altura disponível
             ),
           ),
         ),
